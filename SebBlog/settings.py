71750eb60a05f46bce220c2feeb77f48bc99c82b
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2016-10-19 04:37:52 -0700

    db
    db

--- SebBlog/settings.py
+++ SebBlog/settings.py
@@ -75,19 +75,19 @@ WSGI_APPLICATION = 'SebBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-         # 'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+# DATABASES = {
+#     'default': {
+#          # 'ENGINE': 'django.db.backends.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 }
 
 import dj_database_url
