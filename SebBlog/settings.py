e1a9aa19dad701925178210a11c3e6495111989c
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2016-10-19 03:25:12 -0700

    l
    l

--- SebBlog/settings.py
+++ SebBlog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dk!1v)*@$r5$)t%tjl))+ss)rtnfsc8w^94!7!=jx87d&hxr)u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
