9fd3ab307fb9787bb61b6a911654e30cc4fd75e6
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2016-10-19 21:43:15 -0700

    d
    d

--- SebBlog/settings.py
+++ SebBlog/settings.py
@@ -131,14 +131,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static/'),
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
 
-# ]
+]
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATIC_ROOT = 'staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+# STATIC_ROOT = 'staticfiles/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
